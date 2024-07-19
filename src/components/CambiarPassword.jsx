73d77b5edcee7fa3ac6f5101ab76d40533f022ff
Ivan Silva <silvaivan280@gmail.com>
2024-07-19 14:26:39 -0300

    agregue la logica para cambiar el password del alumno

--- src/components/CambiarPassword.jsx
+++ src/components/CambiarPassword.jsx
@@ -1,6 +1,39 @@
 import { Button, Container, Form } from "react-bootstrap";
+import { useForm } from "react-hook-form";
+import { cambiarPasswordAlumno } from "../components/helpers/queries";
+import Swal from "sweetalert2";
+import { NavLink, useNavigate } from "react-router-dom";
 
 const CambiarPassword = () => {
+  const {register, handleSubmit, formState: {errors}, reset} = useForm(); 
+
+  const navegacion = useNavigate();
+
+  const onSubmit  = (datos) =>{
+    cambiarPasswordAlumno(datos).then((respuesta) =>{
+          if(respuesta){
+              Swal.fire({
+                  title: "Exito!",
+                  text: "La contraseña se modifico correctamente!",
+                  icon: "success"
+                });
+                reset();
+                navegacion("/inicio");
+          }else if(respuesta === null){
+              Swal.fire({
+                  title: "Error!",
+                  text: "Contraseña actual incorrecta!",
+                  icon: "error"
+                });
+          }else{
+              Swal.fire({
+                  title: "Error!",
+                  text: "No se puede cambiar la contraseña, intente nuevamente más tarde!",
+                  icon: "error"
+                });
+          }
+      })
+  }
   return (
     <main className="my-5">
       <Container>
@@ -8,16 +41,28 @@ const CambiarPassword = () => {
           <h3>Cambiar Contraseña</h3>
           <hr />
         </section>
+        <section className="my-3 text-end">
+          <NavLink className="btn btn-regresar" to={"/inicio"}>
+            Volver
+          </NavLink>
+        </section>
         <section>
-          <Form>
+          <Form onSubmit={handleSubmit(onSubmit)}>
             <Form.Group className="mb-3" controlId="formBasicPassword">
               <Form.Label>Contraseña Actual:</Form.Label>
               <Form.Control
                 type="password"
                 placeholder="********"
+                {...register("passwordActual",{
+                  required: "La contraseña es un dato obligatorio",
+                  pattern:{
+                    value: /^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/,
+                    message: "La contraseña debe tener al entre 8 y 16 caracteres, al menos un dígito, al menos una minúscula y al menos una mayúscula."
+                  }
+                })}
               />
               <Form.Text className="text-danger">
-                La contraseña es un campo obligatorio
+              {errors.password?.message}
               </Form.Text>
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicPasswordNuevo">
@@ -25,12 +70,19 @@ const CambiarPassword = () => {
               <Form.Control
                 type="password"
                 placeholder="********"
+                {...register("passwordNuevo",{
+                  required: "La contraseña es un dato obligatorio",
+                  pattern:{
+                    value: /^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/,
+                    message: "La contraseña debe tener al entre 8 y 16 caracteres, al menos un dígito, al menos una minúscula y al menos una mayúscula."
+                  }
+                })}
               />
               <Form.Text className="text-danger">
-                La contraseña nueva es un campo obligatorio
+              {errors.password?.message}
               </Form.Text>
             </Form.Group>
-            <Button type="button">Guardar</Button>
+            <Button type="submit" className="btn btn-login" >Guardar</Button>
           </Form>
         </section>
       </Container>
